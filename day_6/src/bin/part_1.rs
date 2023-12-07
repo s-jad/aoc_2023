6d027a9956f6b7a429c00b74eae76dcdcaa84dba
s-jad <s_jad@web.de>
2023-12-07 07:55:40 +0100

    Feat: quadratic sol for part 1 day 6

--- day_6/src/bin/part_1.rs
+++ day_6/src/bin/part_1.rs
@@ -1,42 +1,39 @@
-fn parse(input: &str) -> [[usize; 4]; 2] {
-    input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .filter(|s| s.chars().all(char::is_numeric))
-                .map(|n| n.parse::<usize>().expect("Should be number"))
-                .collect::<Vec<usize>>()
-                .try_into()
-                .expect("Should be of length 4")
-        })
-        .collect::<Vec<[usize; 4]>>()
-        .try_into()
-        .expect("Should be 2 lines")
+fn solve(time: u64, distance: u64) -> u64 {
+    let t = time as f64;
+    let d = distance as f64;
+
+    let eq = ((t * t - (4.0 * (-d) * -1.0)).sqrt()) / -2.0;
+
+    let lower_bound = (-t / -2.0) + eq;
+    let upper_bound = (-t / -2.0) - eq;
+
+    if lower_bound.fract() == 0.0 || upper_bound.fract() == 0.0 {
+        (upper_bound - lower_bound) as u64 - 1
+    } else {
+        upper_bound.floor() as u64 - lower_bound.ceil() as u64 + 1
+    }
 }
 
 // returns the product from the numbers of ways
 // that you can beat each race.
 // E.g. 4 ways to beat race_1, 3 to beat race_2, 2 to beat race_3 =>
 // 4 * 3 * 2 = 24
-fn process(input: &str) -> usize {
-    let [times, distance] = parse(input);
+fn process(input: &str) -> u64 {
+    let mut lines = input.lines();
 
-    times
-        .iter()
-        .enumerate()
-        .map(|(idx, &t)| {
-            let mut ways = 0;
-            for remaining_time in (0..t).rev() {
-                let speed = t - remaining_time;
-
-                let disance_travelled = speed * remaining_time;
-
-                if disance_travelled > distance[idx] {
-                    ways += 1;
-                }
-            }
-            ways
-        })
+    lines
+        .next()
+        .unwrap_or("")
+        .split(|c: char| !c.is_digit(10))
+        .filter_map(|n| n.parse().ok())
+        .zip(
+            lines
+                .next()
+                .unwrap_or("")
+                .split(|c: char| !c.is_digit(10))
+                .filter_map(|n| n.parse().ok()),
+        )
+        .map(|(time, distance)| solve(time, distance))
         .product()
 }
 
